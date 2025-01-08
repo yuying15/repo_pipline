6008c0aec47a58657570bb8dcf50abbf5b6d89d0
Jane Doe <bda.vic.ca@gmail.com>
2025-01-08 17:07:38 -0500

    new update2

--- dags/s3_to_snowflake_pg_dag.py
+++ dags/s3_to_snowflake_pg_dag.py
@@ -7,21 +7,22 @@ from etl.ETL_S3_SNOWFLAKES import execute_snowflake_sql
 from etl.sf_to_postgresql import sf_to_postgresql
 
 with DAG(
-    dag_id="excel_to_snowflake",
+    dag_id="s3_snowflake_postgresql",
     start_date=datetime(2023, 1, 1),
     schedule_interval="@daily",
     catchup=False
 ) as dag:
-    etl_task = PythonOperator(
-        task_id="excel_to_snowflake_etl",
-        python_callable=excel_to_snowflake_etl,
+
+    execute_sql_task = PythonOperator(
+        task_id="execute_snowflake_sql",
+        python_callable=execute_snowflake_sql
     )
 
-    new_task = PythonOperator(
-        task_id="new_task",
-        python_callable=new_task_function
+    sf_to_pg_task = PythonOperator(
+        task_id="sf_to_postgresql",
+        python_callable=sf_to_postgresql
     )
 
     # Set task dependencies
-    etl_task >> new_task
 
+    execute_sql_task >> sf_to_pg_task
